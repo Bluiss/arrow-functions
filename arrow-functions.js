2cdba038a82201b22f4892c7b259d69e28ddbe8a
Bluiss <135335614+Bluiss@users.noreply.github.com>
2023-12-04 21:54:02 +0000

    add

--- arrow-functions.js
+++ arrow-functions.js
@@ -2,10 +2,24 @@
  * To run this file in Gitpod, use the 
  * command node arrow-functions.js in the terminal
  */
+https://github.com/Bluiss/arrow-functions.git
 
-// Vanilla JavaScript Function
+
+// arrow-functions.js
+const addTwoNumbers = (a, b) => {
+    // code block
+    return a + b;
+};
+
+let sum = addTwoNumbers(1, 2);
+console.log(sum);
+
+
 
 // Arrow Function With Parameters
+
+
+
 
 // Single Line Arrow Function With Parameters
 
